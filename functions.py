77277e58992d31edaf66948cf082619aa0e98bd7
Séraphin Vandegar <sva@opinum.com>
2018-12-29 19:53:56 -0800

    added logs and metrics

--- functions.py
+++ functions.py
@@ -5,8 +5,15 @@ from selenium.common.exceptions import TimeoutException
 import json
 import random
 import time
+from settings import *
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
 
-def wait_element(browser, element, timeout = 20):
+""" Scrapping """
+
+
+def wait_element(browser, element, timeout=20):
     """ Wait till the element is loaded, then select the element.
         if not loaded after 20 sec, throw Timeout error
     """
@@ -18,18 +25,32 @@ def wait_element(browser, element, timeout = 20):
         print("Timed out waiting for page to load")
         return False
 
-def find_element(browser, XPATH : str, timeout = 20):
-    """ Wait till the element is loaded, then select the element.
+
+def find_element(browser, XPATH: str, timeout=20):
+    """ Wait till the element is loaded, then returns the element.
         if not loaded after 20 sec, throw Timeout error
     """
     try:
         WebDriverWait(browser, timeout).until(
-            EC.visibility_of_element_located((By.XPATH,XPATH)))
+            EC.visibility_of_element_located((By.XPATH, XPATH)))
         return browser.find_element_by_xpath(XPATH)
     except TimeoutException:
         print("Timed out waiting for page to load")
         return False
 
+def find_elements(browser, XPATH: str, timeout=20):
+    """ Wait till the elements are loaded, then returns the elements.
+        if not loaded after 20 sec, throw Timeout error
+    """
+    try:
+        WebDriverWait(browser, timeout).until(
+            EC.visibility_of_element_located((By.XPATH, XPATH)))
+        return browser.find_elements_by_xpath(XPATH)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+        return False
+
+""" Files """
 
 
 def read_json_file(file):
@@ -38,11 +59,55 @@ def read_json_file(file):
         result = json.load(file)
         return result
 
-def random_sleep(min =2, max =5):
-    time.sleep(random.randint(min,max))
 
 def write_json_file(data: dict, filename: str):
     """convert the input data dict to JSON
     and write it to a json with the input filename"""
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file)
+
+
+""" Logging """
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_file_handler():
+    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+    logger.propagate = False
+    return logger
+
+class Counters:
+    def __init__(self):
+        self.counters = {}
+    def increment(self,name = 'global',increment_value =1):
+        if name in self.counters:
+            self.counters[name] += increment_value
+        else :
+            self.counters[name] = increment_value
+    def reset(self,name):
+        self.counters[name] = 0
+
+""" Randomization """
+
+def random_sleep(min=2, max=5, logger = None, counter = None):
+    sleeptime = random.randint(min, max)
+    time.sleep(sleeptime)
+    if counter:
+        counter.increment('Sleeptime', sleeptime)
+    if logger:
+        logger.debug('Sleep :' + str(sleeptime))
+    return sleeptime
--- insta_connector.py
+++ insta_connector.py
@@ -3,57 +3,88 @@ import random
 
 
 class Session:
-    def __init__(self, username, password, browser, rules):
+    def __init__(self, username, password, browser, rules, logs = None):
         self.username = username
         self.password = password
         self.browser = browser
         self.rules = rules
         self.timeout = rules['global']['timeout']
+        self.logs = logs
+
 
     def connect(self):
+        logger = self.logs['logger']
+        counter = self.logs['counter']
+        rules_connect = self.rules['connect']
         self.browser.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
         # Find username, password and login input fields
         input_username = fn.find_element(self.browser, "//input[@name='username']", timeout=self.timeout)
         input_password = fn.find_element(self.browser, "//input[@name='password']", timeout=self.timeout)
         login_button = fn.find_element(self.browser, "//button[text()='Log in']", timeout=self.timeout)
-        fn.random_sleep(2, 5)
+        fn.random_sleep(**rules_connect['delay'], **self.logs)
 
         # Fill username and password and login
         input_username.send_keys(self.username)
         input_password.send_keys(self.password)
+        logger.info('Connection to the account :' + self.username)
+        counter.increment('Connection')
         login_button.click()
-        fn.random_sleep(2, 5)
+        fn.random_sleep(**rules_connect['delay'], **self.logs)
 
         # Deny app download if asked
         deny_app_download_button = fn.find_element(self.browser, "//a[text()='Not Now']", self.timeout)
+        logger.debug('App download denied')
         deny_app_download_button.click()
 
         # Deny notifications in browser for current session
         deny_notifications_button = fn.find_element(self.browser, "//button[text()='Not Now']", self.timeout)
         deny_notifications_button.click()
-        fn.random_sleep(2, 5)
+        logger.debug('Notifications denied')
+        fn.random_sleep(**rules_connect['delay'], **self.logs)
 
-    def like(self, hashtags: list, clicked_links : list):
+    def like(self, hashtags: list, used_links: list):
+        # TODO : split in sub-functions
         rules_like = self.rules['like']
+        browser = self.browser
+        logger = self.logs['logger']
+        counter = self.logs['counter']
         random.shuffle(hashtags)
+        logger.debug('Hashtags order: ' + str(hashtags))
+        # loop through the list of hashtags
         for hashtag in hashtags:
-            browser = self.browser
             browser.get("https://www.instagram.com/explore/tags/" + hashtag)
-            for _ in range(*rules_like['count'].values()):
-                picture = fn.find_element(browser,"//a/div/div[1]/img")
-                a = picture.find_elements_by_xpath("//ancestor::a")
-                # if fn.wait_element(browser,a,self.timeout):
+            # loop through a random number of pictures. Range defined in the rule 'postsPerHashtag'
+            for _ in range(random.randint(*rules_like['postsPerHashtag'].values())):
+                # TODO : refactor two next lines to get only pictures instead of every lines
+                picture = fn.find_element(browser, "//a/div/div[1]/img")
+                a = fn.find_elements(picture, "//ancestor::a")
+                counter.increment('Links_founds', len(a))
                 link = a[random.randint(0, len(a))].get_attribute("href")
-                print('got link ' + link)
-                if link not in clicked_links:
+                logger.debug('Got link ' + link)
+                # check whether the link has already been clicked
+                if link not in used_links:
+                    logger.debug('Open link: ' + link)
+                    counter.increment('Links_opened')
                     browser.get(link)
-                    fn.random_sleep(**rules_like['delay'])
-                    heart = fn.find_element(browser, "//span[@aria-label='Like']")
-                    if heart:  # click only if not already liked
-                        if random.random() > rules_like['probability']:
-                            heart.click()
-                    fn.random_sleep(**rules_like['delay'])
-                    clicked_links.append(link)
-                browser.back()
-                fn.random_sleep(**rules_like['delay'])
-        return clicked_links
+                    fn.random_sleep(**rules_like['delay'], **self.logs)
+                    # check if already liked
+                    like_button = fn.find_element(browser, "//span[@aria-label='Like']")
+                    if like_button:
+                        counter.increment('Not_already_liked_post_opened')
+                        rand = random.random()
+                        # like only certain pictures, following the probability set in rules
+                        if rand <= rules_like['probability']:
+                            logger.debug(str(rand) + ' <= ' + str(rules_like['probability']))
+                            logger.info("Like picture")
+                            counter.increment('Post_liked')
+                            like_button.click()
+                        else:
+                            logger.debug(str(rand) + ' > ' + str(rules_like['probability']))
+                            logger.info("Don't like picture")
+                            counter.increment('Post_not_liked')
+                    # back to previous page
+                    fn.random_sleep(**rules_like['delay'], **self.logs)
+                    used_links.append(link)
+                    browser.back()
+                fn.random_sleep(**rules_like['delay'], **self.logs)
+        return counter
