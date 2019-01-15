c7b08e32222a68f86c3a6b20c6780aa0d0d42c25
Séraphin Vandegar <sva@opinum.com>
2019-01-14 18:56:58 -0800

    check user in user collection before checking in acocunts

--- instagram/main.py
+++ instagram/main.py
@@ -16,9 +16,10 @@ def main(username: str, connect=False, like_from_hashtags=False,get_followers = 
     logger = logging.getLogger(__name__)
     if debug:
              logging._handlers['console'].setLevel('DEBUG')
-    logger.info('start script')
+    logger.info('Start script')
     try:
         logger.debug('Environment = ' + os.environ['ENVIRONMENT'])
+        logger.debug('Environment is set to: '+ os.environ['ENVIRONMENT'])
     except KeyError:
         logger.debug('No ENVIRONMENT variable set')
 
@@ -32,65 +33,74 @@ def main(username: str, connect=False, like_from_hashtags=False,get_followers = 
     chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
     browser = webdriver.Chrome(CHROMEDRIVER_PATH, options=chrome_options)
 
-    # get account information
-    logger.debug('get account information')
-    account = mongo.Accounts.objects(username=username).first()
+    # check user credentials
+    #TODO : check user credentials
+    user = mongo.Users.objects(username = username)
     try:
-        if account:
-            credentials = dict(username=account.insta_username, password=account.insta_password)
-        else:
-            raise ValueError('No account found for the username: ' + str(username))
+        if not user:
+            raise ValueError('This user is not existing: ' + username)
     except ValueError as e:
-        logger.error(e)
+        logger.warning(e)
     else:
-        # get user rules, history and user inputs
-        logger.debug('get user rules, history and user_inputs')
-        rules = mongo.Rules.objects(username=username).first()
-        history = mongo.History.objects(username=username).first()
-        user_inputs = mongo.UserInputs.objects(username=username).first()
+        # get account information
+        logger.debug('get account information')
+        account = mongo.Accounts.objects(user.username).first()
+        try:
+            if account:
+                credentials = dict(username=account.insta_username, password=account.insta_password)
+            else:
+                raise ValueError('No account found for the user: ' + str(username))
+        except ValueError as e:
+            logger.warning(e)
+        else:
+            # get user rules, history and user inputs
+            logger.debug('get user rules, history and user_inputs')
+            rules = mongo.Rules.objects(username=username).first()
+            history = mongo.History.objects(username=username).first()
+            user_inputs = mongo.UserInputs.objects(username=username).first()
 
-        # open session
-        logger.debug('open session')
-        session = ifn.Session(credentials, browser, rules, history)
+            # open session
+            logger.debug('open session')
+            session = ifn.Session(credentials, browser, rules, history)
 
-        if connect: # while testing, no need to reconnect every time
-            logger.info('connect to Instagram')
-            session.connect()
+            if connect: # while testing, no need to reconnect every time
+                logger.info('connect to Instagram')
+                session.connect()
 
-        # scripts
-        if like_from_hashtags:
-            try:
-                logger.info('Start like_from_hashtags')
-                counters = session.like_from_hashtags(user_inputs.hashtags)
-                logger.info('End like_from_hashtags')
+            # scripts
+            if like_from_hashtags:
+                try:
+                    logger.info('Start like_from_hashtags')
+                    counters = session.like_from_hashtags(user_inputs.hashtags)
+                    logger.info('End like_from_hashtags')
 
-                # save like metrics
-                metrics = mongo.Metrics(username=username,
-                                        insta_username=session.username,
-                                        datetime=session.start_time,
-                                        sleeptime=counters['sleeptime'],
-                                        connection=counters['connection'],
-                                        links_opened=counters['links_opened'],
-                                        new_post_opened=counters['new_post_opened'],
-                                        post_liked=counters['post_liked'],
-                                        post_not_liked=counters['post_not_liked'],
-                                        execution_time=counters['execution_time'],
-                                        )
-                metrics.save()
-                logger.info('Metrics saved')
+                    # save like metrics
+                    metrics = mongo.Metrics(username=username,
+                                            insta_username=session.username,
+                                            datetime=session.start_time,
+                                            sleeptime=counters['sleeptime'],
+                                            connection=counters['connection'],
+                                            links_opened=counters['links_opened'],
+                                            new_post_opened=counters['new_post_opened'],
+                                            post_liked=counters['post_liked'],
+                                            post_not_liked=counters['post_not_liked'],
+                                            execution_time=counters['execution_time'],
+                                            )
+                    metrics.save()
+                    logger.info('Metrics saved')
 
-                # update history
-                mongo.History.objects(insta_username=session.username).first().modify(
-                    clicked_links=session.clicked_links,
-                    accounts_counter=session.accounts_counter.get_list())
-                logger.info('History saved')
+                    # update history
+                    mongo.History.objects(insta_username=session.username).first().modify(
+                        clicked_links=session.clicked_links,
+                        accounts_counter=session.accounts_counter.get_list())
+                    logger.info('History saved')
 
-            except:
-                logger.error('like_from_hashtags ended unexpectedly')
+                except:
+                    logger.error('like_from_hashtags ended unexpectedly')
 
-        if get_followers:
-            #TODO : add the get_followers function usage here
-            pass
+            if get_followers:
+                #TODO : add the get_followers function usage here
+                pass
 
     logger.info('End of script')
 
